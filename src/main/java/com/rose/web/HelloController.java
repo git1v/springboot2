74ba159a167d8bbf69f234ff876203eaa1018bc2
dean <git1v@yahoo.co.jp>
2017-07-02 13:44:12 +0900

    made form

--- src/main/java/com/rose/web/HelloController.java
+++ src/main/java/com/rose/web/HelloController.java
@@ -3,14 +3,26 @@ package com.rose.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping("/")
+	//@RequestMapping(value="/", method=RequestMethod.GET)
+	@RequestMapping(value="/",method=RequestMethod.GET)
 	public String index(Model model){
-		model.addAttribute("title", "Practice Page");
+		model.addAttribute("title", "Hello Page");
 		model.addAttribute("message", "this data was put on Controller");
 		return "helo";
 	}
+	
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public String form(@RequestParam("input1")String input1, Model model){
+		String res="あなたは「"+input1+"」と入力しました。";
+		model.addAttribute("title", "AnswerPage");
+		model.addAttribute("message", res);
+		return "helo";
+	}
+	
 }
