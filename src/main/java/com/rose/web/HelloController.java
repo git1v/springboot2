5180ce40541435431b394313503de455c29e3d1b
dean <git1v@yahoo.co.jp>
2017-07-02 13:17:29 +0900

    made Controller

---
+++ src/main/java/com/rose/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.rose.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index(){
+		return "this is first test page";
+	}
+}
